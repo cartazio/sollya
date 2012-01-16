e848a70d5079a22fabe92498a6bba4ce70b1cc9c
schevill <schevill@af37bc7e-59aa-4313-bbaf-7d19c0032f32>
2012-01-16 16:13:08 +0000

    * Some other tests.
    This commit was extracted from SVN r1515.
    
    git-svn-id: svn://scm.gforge.inria.fr/svn/sollya/branches/sollya_lib@1515 af37bc7e-59aa-4313-bbaf-7d19c0032f32

---
+++ tests-lib/tfree_variable.c
@@ -0,0 +1,11 @@
+#include <sollya.h>
+
+int main(void) {
+  sollya_obj_t f;
+
+  int sollya_lib_init();
+  f = sollya_lib_free_variable();
+  sollya_lib_printf("%b", f);
+  int sollya_lib_close();
+  return 0;
+}
