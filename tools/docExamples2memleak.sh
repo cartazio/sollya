78d3de444aabddc2205470f5bc6fb438dc4996c2
schevill <schevill@af37bc7e-59aa-4313-bbaf-7d19c0032f32>
2012-12-11 16:04:58 +0000

    * Script to convert all examples of the doc into sollya script files (one per example).
    git-svn-id: svn://scm.gforge.inria.fr/svn/sollya/dev@1878 af37bc7e-59aa-4313-bbaf-7d19c0032f32

---
+++ tools/docExamples2memleak.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]
+  then dir="."
+  else dir=$1
+fi
+
+dir_bak=`pwd`
+cd $dir
+
+if ! /bin/ls *.shlp 1> /dev/null 2>&1
+then
+  echo "Usage $0 dir where dir is the directory containing shlp files"
+  exit 1
+fi
+
+touch ___toto && rm ___toto
+for i in *.shlp
+do
+  grep '#EXAMPLE' -A1000 $i >> ___toto
+done
+grep -v "SEEALSO" ___toto | tr -d '\n' | sed -n 's/#EXAMPLE/\n/g;p' > ___titi
+mv ___titi ___toto
+i=1
+while [ $i -le `wc -l toto |sed -n 's/[^0-9]//g;p'` ]
+do
+  head -n $i ___toto |tail -n 1 > $dir_bak/$i.memleak
+  i=`expr $i + 1`
+done
+rm ___toto
+
+cd $dir_bak
