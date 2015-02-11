39bb5ae408e788ab64d0f267a090a374fd0b6326
Christoph Lauter <christoph.lauter@lip6.fr>
2015-02-11 15:10:18 +0100

    Added testing for sollya_lib_init_with_arguments().

--- tests-lib/tinit_with_arguments.c
+++ tests-lib/tinit_with_arguments.c
@@ -1,19 +1,31 @@
 #include <sollya.h>
 
 int callback(sollya_msg_t msg, void *data) {
-  int message = sollya_lib_get_msg_id(msg);
+  (void)msg; /* Avoiding "unused parameter" warning */
   (void)data; /* Avoiding "unused parameter" warning */
-
-  sollya_lib_printf("Unexpected message\n");
   return 0;
 }
 
 int main(void) {
+  char *argv[] = { "5", "Hello world", "exp(x)", "", "Coucou" };
+  int argc = 5;
+  sollya_obj_t temp;
 
   sollya_lib_init();
   sollya_lib_install_msg_callback(callback, NULL);
 
-  /* TODO */
+  temp = sollya_lib_parse_string("__argv");
+  sollya_lib_printf("__argv = %b\n", temp);
+  sollya_lib_clear_obj(temp);
+
+  sollya_lib_close();
+
+  sollya_lib_init_with_arguments(argc, argv);
+  sollya_lib_install_msg_callback(callback, NULL);
+
+  temp = sollya_lib_parse_string("__argv");
+  sollya_lib_printf("__argv = %b\n", temp);
+  sollya_lib_clear_obj(temp);
 
   sollya_lib_close();
   return 0;
