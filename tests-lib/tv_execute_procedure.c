3db847ea4cb6587d11e26a902266f8566a0970c7
Sylvain Chevillard <sylvain.chevillard@ens-lyon.org>
2016-04-11 15:33:26 +0200

    Added some complementary tests for execute_procedure and updated TODO file.

--- tests-lib/tv_execute_procedure.c
+++ tests-lib/tv_execute_procedure.c
@@ -17,7 +17,7 @@ int callback(sollya_msg_t msg, void *data) {
 }
 
 int main(void) {
-  sollya_obj_t proc1, proc2, proc3, one, two, three, proc1Res, proc2Res, proc3Res;
+  sollya_obj_t proc1, proc2, proc3, one, two, three, proc1Res, proc1Resb, proc1Resc, proc2Res, proc3Res, proc3Resb;
 
   sollya_lib_init();
   sollya_lib_install_msg_callback(callback, NULL);
@@ -31,16 +31,25 @@ int main(void) {
   three = SOLLYA_CONST_SI64(3);
 
   proc1Res = stupid_wrapper(proc1, one, two, NULL);
+  proc1Resb = stupid_wrapper(proc1, one, NULL);
+  proc1Resc = stupid_wrapper(proc1, one, two, three, NULL);
   proc2Res = stupid_wrapper(proc2, NULL);
   proc3Res = stupid_wrapper(proc3, one, two, three, NULL);
+  proc3Resb = stupid_wrapper(proc3, NULL);
 
   sollya_lib_printf("proc1 = %b, args = %b, %b: result = %b\n", proc1, one, two, proc1Res);
+  sollya_lib_printf("proc1 = %b, args = %b: result = %b\n", proc1, one, proc1Resb);
+  sollya_lib_printf("proc1 = %b, args = %b, %b, %b: result = %b\n", proc1, one, two, three, proc1Resc);
   sollya_lib_printf("proc2 = %b: result = %b\n", proc2, proc2Res);
   sollya_lib_printf("proc3 = %b, args = %b, %b, %b: result = %b\n", proc3, one, two, three, proc3Res);
+  sollya_lib_printf("proc3 = %b: result = %b\n", proc3, one, two, three, proc3Resb);
 
   sollya_lib_clear_obj(proc3Res);
+  sollya_lib_clear_obj(proc3Resb);
   sollya_lib_clear_obj(proc2Res);
   sollya_lib_clear_obj(proc1Res);
+  sollya_lib_clear_obj(proc1Resb);
+  sollya_lib_clear_obj(proc1Resc);
   sollya_lib_clear_obj(three);
   sollya_lib_clear_obj(two);
   sollya_lib_clear_obj(one);
