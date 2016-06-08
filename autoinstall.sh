5487b5bab7f9af04bf698a4359e1fee7faabc3bb
Sylvain Chevillard <sylvain.chevillard@ens-lyon.org>
2016-06-08 15:06:27 +0200

    Updated autoinstall script. We now host our own fplll archive server

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
+# Usage: if you wish to use a particular version of GMP, MPFR, MPFI,
+# FPLLL or Sollya, set the following variables to the name of the
+# package. If the variable is left empty, the standard version
+# available on the system will be used.  The special package name
+# 'sollya-git' is available for downloading sollya from the git
+# repository.
 GMP_PKG_NAME=gmp-5.1.3
 MPFR_PKG_NAME=mpfr-3.1.2
 MPFI_PKG_NAME=mpfi-1.5.1
 FPLLL_PKG_NAME=libfplll-4.0.4
 SOLLYA_PKG_NAME=sollya-4.1
 
+# If $LOG_DIR is empty, the current directory will be used.  If
+# $PREFIX_PATH is empty, everything will be installed in the standard
+# place of the system
 LOG_DIR=$HOME/log
 PREFIX_PATH=$HOME/local
 
