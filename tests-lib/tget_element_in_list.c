aa86a6f37bd3ce7583cb3a06d2b11ae963c30320
Sylvain Chevillard <sylvain.chevillard@ens-lyon.org>
2016-06-14 19:22:55 +0200

    Made the timing tests for tget_element_in_list more robust.

--- tests-lib/tget_element_in_list.c
+++ tests-lib/tget_element_in_list.c
@@ -1,19 +1,23 @@
 #include <sollya.h>
+#include <inttypes.h>
 
 typedef struct __sollya_time_struct_t sollya_time_t;
 int sollya_gettime(sollya_time_t *);
 int64_t sollya_timediff_ms(sollya_time_t *, sollya_time_t *);
 sollya_time_t *sollya_gettime_var();
 
+#define LIST_SIZE 8000
+
 int main(void) {
   sollya_obj_t listobj;
-  sollya_obj_t tab[8000];
+  sollya_obj_t tab[LIST_SIZE];
   sollya_obj_t a;
   int i, k;
   sollya_time_t *t1;
   sollya_time_t *t2;
   int64_t length1;
   int64_t length2;
+  int64_t nb_iter;
 
   sollya_lib_init();
 
@@ -276,24 +280,41 @@ int main(void) {
 
 
   /* Time Complexity */
-  for(i=0;i<8000;i++) tab[i] = SOLLYA_CONST(i);
+  for(i=0;i<LIST_SIZE;i++) tab[i] = SOLLYA_CONST(i);
 
-  listobj = sollya_lib_list(tab, 4000);
   t1 = sollya_gettime_var();
   t2 = sollya_gettime_var();
+
+  listobj = sollya_lib_list(tab, (LIST_SIZE/2));
+  nb_iter = 1024;
+  do {
+    nb_iter = nb_iter*2;
+    sollya_gettime(t1);
+    for(k=0;k<nb_iter;k++) {
+      sollya_lib_get_element_in_list(&a, listobj, (LIST_SIZE/2)-1);
+      sollya_lib_clear_obj(a);
+    }
+    sollya_gettime(t2);
+    length1 = sollya_timediff_ms(t1,t2);
+  } while ( (nb_iter < INT64_MAX/2) && (length1 < 1000)); /* The loop must take at least 1000 ms */
+  sollya_lib_clear_obj(listobj);
+
+  /* Measures time on list 1 */
+  listobj = sollya_lib_list(tab, (LIST_SIZE/2));
   sollya_gettime(t1);
-  for(k=0;k<100000;k++) {
-    sollya_lib_get_element_in_list(&a, listobj, 3999);
+  for(k=0;k<nb_iter;k++) {
+    sollya_lib_get_element_in_list(&a, listobj, (LIST_SIZE/2)-1);
     sollya_lib_clear_obj(a);
   }
   sollya_gettime(t2);
   length1 = sollya_timediff_ms(t1,t2);
   sollya_lib_clear_obj(listobj);
 
-  listobj = sollya_lib_list(tab, 8000);
+  /* Measures time on list 2 */
+  listobj = sollya_lib_list(tab, LIST_SIZE);
   sollya_gettime(t1);
-  for(k=0;k<100000;k++) {
-    sollya_lib_get_element_in_list(&a, listobj, 7999);
+  for(k=0;k<nb_iter;k++) {
+    sollya_lib_get_element_in_list(&a, listobj, LIST_SIZE-1);
     sollya_lib_clear_obj(a);
   }
   sollya_gettime(t2);
@@ -305,9 +326,9 @@ int main(void) {
   if ((0.75 <= ((double)length2)/((double)length1)) && (((double)length2)/((double)length1) <= 1.25))
     sollya_lib_printf("Testing that a call to get_element_in_list has complexity O(1): OK\n");
   else
-    sollya_lib_printf("Testing that a call to get_element_in_list has complexity O(1): not OK. Observed ratio: %g (should be close to 1 for O(1) complexity, and close to 2 for linear complexity)\n", ((double)length2)/((double)length1));
+    sollya_lib_printf("Testing that a call to get_element_in_list has complexity O(1): not OK. Observed ratio: length1/length2 = %" PRId64 "/%" PRId64 " = %g (should be close to 1 for O(1) complexity, and close to 2 for linear complexity). Number of iterations: nb_iter = %" PRId64 "\n", length1, length2, ((double)length2)/((double)length1), nb_iter);
 
-  for(i=0;i<8000;i++) sollya_lib_clear_obj(tab[i]);
+  for(i=0;i<LIST_SIZE;i++) sollya_lib_clear_obj(tab[i]);
 
  sollya_lib_close();
 
