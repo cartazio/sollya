9d8d873bf9ab5d258abaf333a21932d480217c31
Sylvain Chevillard <sylvain.chevillard@ens-lyon.org>
2016-07-13 12:22:31 +0200

    Better choice for standards compilation flags.

--- autoinstall.sh
+++ autoinstall.sh
@@ -29,8 +29,8 @@ MPFR_CONFIGURE_OPTIONS=""
 MPFI_CONFIGURE_OPTIONS=""
 FPLLL_CONFIGURE_OPTIONS=""
 
-SOLLYA_CFLAGS="-g -g3 -gdwarf-2 -O0 -Wall -Wextra -std=c99"
-SOLLYA_CXXFLAGS="-g -g3 -gdwarf-2 -O0 -Wall -Wextra"
+SOLLYA_CFLAGS="-O2 -Wall -Wextra -std=c99"
+SOLLYA_CXXFLAGS="-O2 -Wall -Wextra"
 
 ###############################################################################
 
