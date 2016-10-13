cartazio/sollya

commit f175f2b938036f38a027adf2f0f381db87bd1e21

Still trying to fix a strange issue with fork() and the input streams used for read.

general.c
@@ -315,17 +315,25 @@ extern int yylex(void *);
 extern void yyset_in(FILE *, void *);
 extern int parserCheckEof();
 
+extern FILE *getCurrentLexerStream();
+
 void freeGlobalReusedMPFIVars();
 void freeGlobalReusedMPFRVars();
 void freeTool();
 
+void parserFlushInput() {
+  fflush(getCurrentLexerStream());
+}
+
 void readManipulate() {
   deferSignalHandling();
   fflush(NULL);
+  parserFlushInput();
   resumeSignalHandling();
   wait(NULL);
   deferSignalHandling();
   fflush(NULL);
+  parserFlushInput();
   resumeSignalHandling();
 }
 
@@ -2636,6 +2644,8 @@ static int general(int argc, char *argv[]) {
 	  deferSignalHandling();
           fflush(stdout);
           fflush(stderr);
+	  fflush(NULL);
+	  parserFlushInput();
 	  resumeSignalHandling();
         }
 	libraryMode = 0;
@@ -2650,6 +2660,8 @@ static int general(int argc, char *argv[]) {
 	  deferSignalHandling();
           fflush(stdout);
           fflush(stderr);
+	  fflush(NULL);
+	  parserFlushInput();
 	  resumeSignalHandling();
         }
 	blockSignals(eliminatePromptBackup);

external.c
@@ -132,6 +132,7 @@ char *evaluateStringAsBashCommand(char *command, char *input) {
       */
       deferSignalHandling();
       fflush(NULL);
+      parserFlushInput();
       resumeSignalHandling();
       if ((pid = fork()) == -1) {
 	/* Error forking */
@@ -268,6 +269,7 @@ char *evaluateStringAsBashCommand(char *command, char *input) {
 
   deferSignalHandling();
   fflush(NULL);
+  parserFlushInput();
   resumeSignalHandling();
 
   return res;
@@ -415,6 +417,7 @@ void externalPlot(char *library, mpfr_t a, mpfr_t b, mp_prec_t samplingPrecision
 
   deferSignalHandling();
   fflush(NULL);
+  parserFlushInput();
   resumeSignalHandling();
   if ((name==NULL) || (type==PLOTFILE)) {
     if (fork()==0) {
@@ -467,8 +470,8 @@ void externalPlot(char *library, mpfr_t a, mpfr_t b, mp_prec_t samplingPrecision
   wait(NULL);
   deferSignalHandling();
   fflush(NULL);
+  parserFlushInput();
   resumeSignalHandling();
-
   
   safeFree(gplotname);
   safeFree(dataname);

general.h
@@ -288,6 +288,7 @@ size_t sollya_fread(void *, size_t, size_t, FILE *);
 size_t sollya_fwrite(const void *, size_t, size_t, FILE *);
 int sollya_gettime(sollya_time_t *);
 void readManipulate();
+void parserFlushInput();
 
 /* Compatibility macros */
 

lexer.l
@@ -551,14 +551,15 @@ NOP             "nop"
                         }
                         promptToBePrinted = 1;
 		      } else {
-			newReadFileStarted();
+                        fflush(yyin);
+                        newReadFileStarted();
 			currStatePtr = (YY_BUFFER_STATE *) safeMalloc(sizeof(YY_BUFFER_STATE));
 			*currStatePtr = YY_CURRENT_BUFFER;
 			readStack = addElement(readStack, (void *) currStatePtr);
 			tempFDPtr = (FILE **) safeMalloc(sizeof(FILE *));
 			*tempFDPtr = yyin;
 			readStack2 = addElement(readStack2, (void *) tempFDPtr);
-			yyin = temp_fd;
+                        yyin = temp_fd;
 			yy_switch_to_buffer(yy_create_buffer(yyin, YY_BUF_SIZE, scanner), scanner);
 		      }
                       safeFree(newReadFilenameTemp);
@@ -580,7 +581,8 @@ NOP             "nop"
 			yyterminate();
 		      } else {
 			fclose(yyin);
-			yyin = *((FILE **) (readStack2->value));
+                        yyin = *((FILE **) (readStack2->value));
+                        fflush(yyin);
 			safeFree(readStack2->value);
 			readStackTemp = readStack2->next;
 			safeFree(readStack2);
@@ -904,6 +906,9 @@ NOP             "nop"
 
 %%
 
+FILE *getCurrentLexerStream() {
+  return yyget_in(scanner);
+}
 
 char *getCurrentLexSymbol() {
   char *str;

plot.c
@@ -309,6 +309,7 @@ void plotTree(chain *treeList, mpfr_t a, mpfr_t b, unsigned long int points, mp_
 
   deferSignalHandling();
   fflush(NULL);
+  parserFlushInput();
   resumeSignalHandling();
   if (plotPossible) {
     if ((name==NULL) || (type==PLOTFILE)) {
@@ -363,6 +364,7 @@ void plotTree(chain *treeList, mpfr_t a, mpfr_t b, unsigned long int points, mp_
   wait(NULL);
   deferSignalHandling();
   fflush(NULL);
+  parserFlushInput();
   resumeSignalHandling();
 
 

commit 0c9e222c9f86cd76cb568184cacf632ae7be37a4

Trying to patch a regression in the Sollya library introduced with commit f175f2b938036f38a027adf2f0f381db87bd1e21

general.c
@@ -309,6 +309,13 @@ int inside_sollya_ferror = 0;
 int inside_sollya_fwrite = 0;
 /* End of global variables for sollya_getc */
 
+/* Global variable for the lexer/parser when the input stream needs to
+   be rewound before/during/after a fork 
+*/
+int parserAlreadyRunning = 0;
+/* End of global variable */
+
+
 extern int yyparse(void *); 
 extern void yylex_destroy(void *);
 extern int yylex_init(void **);
@@ -323,6 +330,7 @@ void freeGlobalReusedMPFRVars();
 void freeTool();
 
 void parserFlushInput() {
+  if (!parserAlreadyRunning) return;
   fflush(getCurrentLexerStream());
 }
 
@@ -1428,6 +1436,7 @@ void freeTool() {
     safeFree(readStack);
     readStack = readStackTemp;
   }
+  parserAlreadyRunning = 0;
   yylex_destroy(scanner);
   freeFunctionLibraries();
   freeConstantLibraries();
@@ -2546,7 +2555,8 @@ static int general(int argc, char *argv[]) {
   if (inputFileOpened) {
     yyset_in(inputFile, scanner);
   }
-
+  parserAlreadyRunning = 1;
+  
   if (!doNotModifyStackSize) {
 #if defined(RLIMIT_STACK)
     if (getrlimit(RLIMIT_STACK,&rlim) == 0) {